ebe4297cd1ec12e826e39aac07c1c29e110eb70a
jnauta <jnautahan@gmail.com>
2017-02-28 18:22:33 +0100

    Adds picture, seems to be working

--- earthquake_mapping/main.js
+++ earthquake_mapping/main.js
@@ -3,6 +3,7 @@
 */
 
 //Set global variables
+var mapImg;
 var data_csv;
 var api_url = "https://api.mapbox.com";
 var style_url = "/styles/v1/mapbox/light-v9/static/";
@@ -12,7 +13,8 @@ var access_token = "?access_token=pk.eyJ1Ijoiam5hdXRhIiwiYSI6ImNpenBvbW0xYTAwMG0
 	/styles/v1/{username}/{style_id}/static/{overlay}/{lon},{lat},{zoom},{bearing},{pitch}{auto}/{width}x{height}{@2x}
 */
 
-function setup() {
+//Preload the map upon which to draw
+function preload() {
 	function specifics() {
 		this.lon = 0;
 		this.lat = 0;
@@ -22,16 +24,15 @@ function setup() {
 	}
 	var specificString = specifics();
 
-	//Generate map overlay call string
 	var mapImg_call = api_url+style_url+specificString+access_token;
-	var mapImg = loadImage(mapImg_call);
-	print(mapImg);
+	mapImg = loadImage(mapImg_call);
+}
 
+function setup() {
 	//Create canvas to draw on
 	createCanvas(windowWidth, windowHeight);
-	// translate(0,0);
+	//Display static image background
 	image(mapImg, 0, 0);
-
 }
 
 function getAPI_data(data) {
